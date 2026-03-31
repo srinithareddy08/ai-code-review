c1d0b13c9d161fceeb1d811b449f759ce9b52a94
Srinitha Reddy <srinithasama6@gmail.com>
2026-03-31 10:35:57 -0400

    Completed the " download reports"

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,4 +5,5 @@ const authController = require("../controllers/authcontroller");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
+
 module.exports = router;
